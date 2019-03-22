32868ec34cfd99e590f9b498196ad0d13f1bd249
leticiamazzoportela <leticiaportela@alunos.utfpr.edu.br>
2019-03-21 21:22:45 -0300

    Finish pruneTree function

--- src/pruneTree.py
+++ src/pruneTree.py
@@ -3,16 +3,28 @@ from anytree.exporter import DotExporter
 
 def prune(tree):
     for node in PostOrderIter(tree):
-        node_name = node.name.split(".") #comeca com id
+        node_name = node.name.split('.') #comeca com id
+        no = node.parent
 
         if (node_name[1] == ':' or node_name[1] == ',' or node_name[1] == '(' or
             node_name[1] == ')' or node_name[1] == '[' or node_name[1] == ']' or
             node_name[1] == ':='):
             node.parent = None
     
+    for node in PostOrderIter(tree):
+        node_name = node.name.split('.') #comeca com id
+        no = node.parent
+
+        if node.parent != None:
+            father = node.parent.name.split('.')
+
+            if node_name[1] == father[1]:
+                node.parent = no.parent
+                if len(no.children) > 0:
+                    no.children[0].parent = node
+                no.parent = None           
+
     showPruneTree(tree)
 
 def showPruneTree(tree):
     DotExporter(tree).to_picture("pruneAst.png")
-
-# TODO Terminar de podar a árvore
